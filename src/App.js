b32649726e3df5de27a3889ece98afff8b0711b5
shashidar mandalozu <sasitech18@gmail.com>
2018-01-05 15:45:49 +0000

    Add necessary files and folders for the project

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class ContactList extends React.Component {
+  render() {
+    const people = this.props.contacts;
+
+    return (
+      <ol>{people.map(person => <li key={person.name}>{person.name}</li>)}</ol>
+    );
+  }
+}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ContactList
+          contacts={[
+            { name: "Shashi" },
+            { name: "Jhansi" },
+            { name: "Shajhanshi" }
+          ]}
+        />
+        <ContactList
+          contacts={[
+            { name: "Chukki" },
+            { name: "Chakki" },
+            { name: "Chakkichukki" }
+          ]}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
