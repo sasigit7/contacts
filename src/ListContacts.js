a8583d415edf6599b79fb7d16e9593ba8f477722
shashidar mandalozu <sasitech18@gmail.com>
2018-01-07 15:28:33 +0000

    Convert ListContacts to a Stateless Functional Component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,4 +1,34 @@
 import React, { Component } from 'react';
+
+// ADD STATE TO A COMPONENT - STATELESS FUNCTIONAL COMPONENTS
+function ListContacts (props) {
+    return (
+      <ol className="contact-list">
+      {props.contacts.map((contact) => (
+         <li key={contact.id} className='contact-list-item'>
+
+           <div className='contact-avatar' style={
+             {
+               backgroundImage: `url(${contact.avatarURL})`
+             }
+           }>
+         </div>
+
+         <div className='contact-details'>
+            <p>{contact.name}</p>
+            <p>{contact.email}</p>
+         </div>
+
+        <button className='contact-remove'>
+          Remove
+        </button>
+         </li>
+      ))}
+    </ol>
+    )
+}
+
+/* PASS DATA WITH PROPS
 class ListContacts extends Component {
    render() {
     // console.log('Props', this.props)
@@ -28,4 +58,5 @@ class ListContacts extends Component {
      )
    }
 }
+*/
 export default ListContacts
